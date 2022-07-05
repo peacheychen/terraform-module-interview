6b2890aa694740bb4fe648d6007f4d2b306167af
peacheychen <chendapeng0015@gmail.com>
2022-07-05 14:19:04 +0100

    update aap

--- app/docker/main.tf
+++ app/docker/main.tf
@@ -1,13 +1,13 @@
-terraform {
-  required_version = ">= 0.1.0.7"
+# terraform {
+#   required_version = ">= 0.1.0.7"
 
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "2.15.0"
-    }
-  }
-}
+#   required_providers {
+#     docker = {
+#       source  = "kreuzwerker/docker"
+#       version = "2.15.0"
+#     }
+#   }
+# }
 
 resource "docker_container" "container" {
   image = var.docker_container_image
