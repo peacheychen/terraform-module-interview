0ea2e7167ea5393c1981ddd91d77deb370fa9dba
peacheychen <chendapeng0015@gmail.com>
2022-07-05 13:00:36 +0100

    add provider

--- app/docker/main.tf
+++ app/docker/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 0.1.0.7"
+
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.15.0"
+    }
+  }
+}
+
 resource "docker_container" "container" {
   image = var.docker_container_image
   name  = var.docker_container_name
